49a0811aaee9e262c55de4ec7a1f3ba2bcb2c2cb
rianvputraa <32771360+rianvputraa@users.noreply.github.com>
2017-11-28 12:15:38 +0700

    Nuansa Asri Residence v.1.0

---
+++ kpr.php
@@ -0,0 +1,120 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="ID">
+<head>
+    <title>Kalkulator KPR</title>
+    <meta http-equiv="Content-Language" content="id">
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <meta name="description" content="Kalkulator KPR Online">
+    <meta name="keywords" content="kalkulator kpr, hitung kpr, simulasi kpr, KPR">
+    <meta name="MSSmartTagsPreventParsing" content="true">
+    <meta name="robots" content="noarchive">
+    <meta name="msnbot" content="nopreview">
+    <link rel="canonical" href="http://rumahdijual.com/kalkulatorkpr.php"/>
+    <style type="text/css">
+        <!--
+        form {
+            margin: 0;
+        }
+
+        form input {
+            font-family: Verdana, Georgia, Arial, sans-serif;
+            font-size: 12px;
+        }
+
+        p {
+            margin-top: 0;
+        }
+
+        a {
+            color: #33C;
+        }
+
+        a:hover {
+            color: #006;
+        }
+
+        a:visited {
+            color: #339;
+        }
+
+        a:visited:hover {
+            color: #006;
+        }
+
+        body {
+            background-color: #FFF;
+            color: #333;
+            margin: 6px 4px 6px 4px;
+            padding: 0;
+            font-family: Verdana, Georgia, Arial, sans-serif;
+            font-size: 12px;
+        }
+
+        .roundbox {
+            border-radius: 6px;
+            -moz-border-radius: 6px;
+            -webkit-border-radius: 6px;
+        }
+
+        -->
+    </style>
+</head>
+<body>
+
+
+<div class="roundbox" style="background-color:#FCFCFC;padding:4px;border:1px solid #D6D6D6;margin-top:8px;">
+    <form action="widgetkpr.php" method="GET" name="ftr">
+
+        <table cellpadding="3" cellspacing="0" border="0">
+            <tr>
+                <td style="white-space:nowrap;padding-bottom:5px;">Jumlah&nbsp;Kredit:</td>
+                <td style="white-space:nowrap;padding-bottom:5px;"><b>Rp.</b>&nbsp;<input type="text" name="a" size="30"
+                                                                                          value=""
+                                                                                          style="width:80px;">
+                </td>
+            </tr>
+            <tr>
+                <td style="white-space:nowrap;padding-bottom:5px;">Jangka&nbsp;Waktu:</td>
+                <td style="white-space:nowrap;padding-bottom:5px;"><input type="text" name="y" size="30"
+                                                                          value=""
+                                                                          style="width:50px;">&nbsp;<b>tahun</b>
+                </td>
+            </tr>
+            <tr>
+                <td style="white-space:nowrap;">Bunga:</td>
+                <td style="white-space:nowrap;"><input type="text" name="i" size="30" value=""
+                                                       style="width:50px;">&nbsp;<b>%</b></td>
+            </tr>
+        </table>
+
+        <div style="margin:6px;margin-left:3px;">
+            <input type="submit" value="Hitung Angsuran">
+        </div>
+
+    </form>
+</div>
+
+
+
+
+    <script type="text/javascript">
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-1170896-94', 'auto');
+        ga('set', 'dimension1', 'Kalkulator');
+        ga('send', 'pageview');
+    </script>
+
+
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs1.uzone.id/2fn7a2/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582HVlH3eBnL31FYCvEPbbClajfQA0rXS2GwxFFoB8rk2ESMBgBNILzXnbuqePi19p7PJUSIzd2mH44ieRATWxmcmIO9jEw7jtAYf7oAFmWhqO%2fnVhQvSXTuESdNpsywNrXbw3ApNx62y%2fK75xEeUwmLkb4pZCEIL0RgeLMpu3Aj68z4gByuEOztCUZ7u%2bknooE8LMnpu%2b4c9deT3nCLWUAXvfp3BD0cLO2e0m3YQzvK3oeGNnItCf%2bHdjxaX2fxnC6gnODooM%2fV%2bBbrxJYVBO8m%2byxHWhasIFZeuvGNEVJD3uchAuovS%2bCGxSWrwqTnxh9RCoyh1%2fjVolL%2f5DotJaNpiwTv5sPTgMOJC4nt%2f3PF1vLlfaMaUHOsD0%2bRiLPfgTPP0qpoRBETycKV5MJQgFoqv7mqr8MqeetI4cKVy12BLU" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
---
+++ widgetkpr.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="ID">
+<head>
+    <title>Kalkulator KPR</title>
+    <meta http-equiv="Content-Language" content="id">
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <meta name="description" content="Kalkulator KPR Online">
+    <meta name="keywords" content="kalkulator kpr, hitung kpr, simulasi kpr, KPR">
+    <meta name="MSSmartTagsPreventParsing" content="true">
+    <meta name="robots" content="noarchive">
+    <meta name="msnbot" content="nopreview">
+    <link rel="canonical" href="http://rumahdijual.com/kalkulatorkpr.php"/>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- font Awesome CSS -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css">
+  <!-- or -->
+  <link rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+
+    <!-- Main Styles CSS -->
+    <link href="css/main.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+
+<form action="widgetkpr.php" id="form" method="GET" class="topBefore" name="ftr">
+        
+          <input id="name" name="a" type="text" placeholder="JUMLAH KREDIT">
+          <input id="email" name="y" type="text" placeholder="JANGKA WAKTU /tahun">
+          <input id="email" name="i" type="text" placeholder="BUNGA">
+          <br>
+  <input id="submit" type="submit" value="CEK KPR">
+  
+</form>
+
+
+
+
+    <script type="text/javascript">
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-1170896-94', 'auto');
+        ga('set', 'dimension1', 'Kalkulator');
+        ga('send', 'pageview');
+    </script>
+
+
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs1.uzone.id/2fn7a2/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582HVlH3eBnL312Bm4I8YgJfegduvmR42wd9ubq3nrBLXENsAfHcgk5KWqzfzcJi24%2fYndYI9YnruRjYwuqh%2bnV4y7x6nCQ%2bibsWrhqF4THmefgnkZZQqal32wLfUfGIJc9dA%2bhpQyqWqHJGDZI5bdbTElWCVhhOQogYd1UCnuCoYs%2bEqtlOte4siqsuNfEa5IzZVaRPwMHNzHKfObY04FdX%2bsoLptCp%2bLINKrkrFX3baY%2baFHWTSVGzFHk8Ac481vOcBxz5e96ezNrMvu1SU78RO1nFzgpzyQx8PNK%2fZ2YXl7sJM5En81knC4OFOrNUARkj2rWvvQQWuNfYRIeaN94DtwrZYDpf%2b1D6CObklC2n4cKO6Yl4LqyYRxNbkHySnwHuvfLIYIx5dMIar0%2fWiv%2bSnnIroqp3KCSBHGQX6Z30RZ" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
